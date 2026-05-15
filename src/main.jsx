44ecbe936f4814ffbef4437356bf182105e6a7fd
Timo_thy0987 <timo@example.com>
2026-05-15 22:56:00 +0100

    fix: define globalThis for Wagmi build

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { Buffer } from 'buffer';
 window.Buffer = window.Buffer || Buffer;
+import '@rainbow-me/rainbowkit/styles.css';
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultConfig, RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit';
 import { WagmiProvider, http } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,7 @@ export default defineConfig({
     react(),
     nodePolyfills()
   ],
+  define: {
+    global: 'globalThis',
+  },
 })
