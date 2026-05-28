c73961fcccb459d48dd430bb333f769a07ee5cb7
Timothy0987 <timothyolashile4@gmail.com>
2026-05-29 00:50:46 +0100

    feat: upgrade ground plane to realistic PBR grass pitch with penalty box markings

--- src/components/3d/Stadium.jsx
+++ src/components/3d/Stadium.jsx
@@ -1,49 +1,121 @@
-import React from 'react';
-import { Ring } from '@react-three/drei';
+import React, { Suspense, useMemo, useEffect } from 'react';
+import { useTexture } from '@react-three/drei';
+import * as THREE from 'three';
+
+const PitchInner = () => {
+  const [colorMap, normalMap, roughnessMap] = useTexture([
+    'https://raw.githubusercontent.com/pmndrs/drei-assets/master/textures/grass/color.jpg',
+    'https://raw.githubusercontent.com/pmndrs/drei-assets/master/textures/grass/normal.jpg',
+    'https://raw.githubusercontent.com/pmndrs/drei-assets/master/textures/grass/roughness.jpg'
+  ]);
+
+  useEffect(() => {
+    [colorMap, normalMap, roughnessMap].forEach((texture) => {
+      if (texture) {
+        texture.wrapS = THREE.RepeatWrapping;
+        texture.wrapT = THREE.RepeatWrapping;
+        texture.repeat.set(24, 24);
+      }
+    });
+  }, [colorMap, normalMap, roughnessMap]);
+
+  const pitchLinesTexture = useMemo(() => {
+    const canvas = document.createElement('canvas');
+    canvas.width = 1024;
+    canvas.height = 1024;
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return null;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    const W = canvas.width;
+    const H = canvas.height;
+
+    // Helper functions to map 3D coordinates [-20, 20] to canvas [0, 1024]
+    const getX = (x3d) => ((x3d + 20) / 40) * W;
+    const getZ = (z3d) => ((z3d + 20) / 40) * H;
+
+    ctx.strokeStyle = 'rgba(255, 255, 255, 0.85)';
+    ctx.lineWidth = 6;
+    ctx.lineCap = 'round';
+    ctx.lineJoin = 'round';
+
+    // 1. Goal Line
+    ctx.beginPath();
+    ctx.moveTo(getX(-20), getZ(-5));
+    ctx.lineTo(getX(20), getZ(-5));
+    ctx.stroke();
+
+    // 2. Penalty Box (18-yard box)
+    // Width = 29.3 units, Depth = from Z=-5 to Z=7
+    ctx.beginPath();
+    ctx.strokeRect(getX(-14.65), getZ(-5), getX(14.65) - getX(-14.65), getZ(7) - getZ(-5));
+
+    // 3. Goal Area (6-yard box)
+    // Width = 13.3 units, Depth = from Z=-5 to Z=-1
+    ctx.beginPath();
+    ctx.strokeRect(getX(-6.65), getZ(-5), getX(6.65) - getX(-6.65), getZ(-1) - getZ(-5));
+
+    // 4. Penalty Spot (Z = 3)
+    ctx.beginPath();
+    ctx.arc(getX(0), getZ(3), 6, 0, 2 * Math.PI);
+    ctx.fillStyle = '#ffffff';
+    ctx.fill();
+
+    // 5. Penalty Arc (D-arc)
+    // Radius = 6.65 units, Centered at [0, 3], extending from Z=7 to Z=9.65
+    const radius = (6.65 / 40) * W;
+    const angleStart = Math.acos(4 / 6.65);
+    const angleEnd = Math.PI - angleStart;
+
+    ctx.beginPath();
+    ctx.arc(getX(0), getZ(3), radius, angleStart, angleEnd);
+    ctx.stroke();
+
+    const tex = new THREE.CanvasTexture(canvas);
+    tex.colorSpace = THREE.SRGBColorSpace;
+    return tex;
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      if (pitchLinesTexture) pitchLinesTexture.dispose();
+    };
+  }, [pitchLinesTexture]);
+
+  return (
+    <group position={[0, 0, 0]}>
+      {/* Ground plane with PBR Grass */}
+      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]} receiveShadow>
+        <planeGeometry args={[100, 100]} />
+        <meshStandardMaterial
+          map={colorMap}
+          normalMap={normalMap}
+          roughnessMap={roughnessMap}
+          roughness={0.8}
+        />
+      </mesh>
+
+      {/* Pitch markings transparent overlay */}
+      {pitchLinesTexture && (
+        <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0.005, 0]} receiveShadow>
+          <planeGeometry args={[40, 40]} />
+          <meshStandardMaterial
+            map={pitchLinesTexture}
+            transparent
+            opacity={0.9}
+            roughness={0.8}
+          />
+        </mesh>
+      )}
+    </group>
+  );
+};
 
 export const Pitch = () => (
-  <group position={[0, 0, 0]}>
-    {/* Dark tech floor */}
-    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.01, 0]} receiveShadow>
-      <planeGeometry args={[100, 100]} />
-      <meshStandardMaterial color="#020510" roughness={0.2} metalness={0.8} />
-    </mesh>
-
-    {/* E-Football Grid Helper */}
-    <gridHelper args={[100, 100, '#103060', '#0a1530']} position={[0, 0, 0]} />
-
-    {/* Glowing Rings around the Penalty Spot (Z=3) */}
-    <group position={[0, 0.01, 3]} rotation={[-Math.PI / 2, 0, 0]}>
-      {/* Center Spot */}
-      <mesh>
-        <circleGeometry args={[0.2, 32]} />
-        <meshBasicMaterial color="#00FFFF" />
-      </mesh>
-      
-      {/* Concentric rings */}
-      <Ring args={[1.5, 1.55, 64]}>
-        <meshStandardMaterial color="#00FFFF" emissive="#00FFFF" emissiveIntensity={2} transparent opacity={0.8} side={2} />
-      </Ring>
-      <Ring args={[3.0, 3.05, 64]}>
-        <meshStandardMaterial color="#00FFFF" emissive="#00FFFF" emissiveIntensity={1} transparent opacity={0.5} side={2} />
-      </Ring>
-      <Ring args={[4.5, 4.55, 64]}>
-        <meshStandardMaterial color="#00FFFF" emissive="#00FFFF" emissiveIntensity={0.5} transparent opacity={0.2} side={2} />
-      </Ring>
-    </group>
-
-    {/* Pitch Boundary Lines */}
-    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0.01, -2]}>
-      <planeGeometry args={[30, 40]} />
-      <meshBasicMaterial color="#00FFFF" wireframe transparent opacity={0.3} />
-    </mesh>
-    
-    {/* Penalty Box Line */}
-    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0.02, -2.5]}>
-      <planeGeometry args={[12, 10]} />
-      <meshBasicMaterial color="#00FFFF" wireframe />
-    </mesh>
-  </group>
+  <Suspense fallback={null}>
+    <PitchInner />
+  </Suspense>
 );
 
 export const Goalpost = () => (
